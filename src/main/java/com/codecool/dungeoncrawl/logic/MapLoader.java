ea33c418ff6f006db0a9b5bd5730691f005387b9
breezh <benii1030@gmail.com>
2023-08-07 10:53:49 +0200

    test

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -18,6 +18,7 @@ public class MapLoader {
 
         scanner.nextLine(); // empty line
 
+
         GameMap map = new GameMap(width, height, CellType.EMPTY);
         for (int y = 0; y < height; y++) {
             String line = scanner.nextLine();
