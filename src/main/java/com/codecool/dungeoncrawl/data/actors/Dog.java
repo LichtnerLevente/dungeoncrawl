20c96c28e70fbb7a420b7a4c786725ca110dc2dd
breezh <benii1030@gmail.com>
2023-08-08 21:53:04 +0200

    toggle follow or not

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Dog.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Dog.java
@@ -4,11 +4,13 @@ import com.codecool.dungeoncrawl.data.Cell;
 
 import java.util.Arrays;
 
-public class Dog extends Actor{
-    private boolean shouldMove = false;
+public class Dog extends Actor {
+    private boolean shouldMove = true;
+
     public Dog(Cell cell) {
         super(cell);
     }
+
     @Override
     public String getTileName() {
         return "dog";
@@ -20,13 +22,15 @@ public class Dog extends Actor{
 
 
     public void moveDog() {
-        Cell nextCell = cell.getCell(prevCoord[0],prevCoord[1]);
+        Cell nextCell = cell.getCell(prevCoord[0], prevCoord[1]);
+        if (shouldMove) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
-        System.out.println(prevCoord[0]);
-        System.out.println(prevCoord[1]);
-        System.out.println(Arrays.toString(prevCoord));
+        }
+//        System.out.println(prevCoord[0]);
+//        System.out.println(prevCoord[1]);
+//        System.out.println(Arrays.toString(prevCoord));
 
     }
 }
