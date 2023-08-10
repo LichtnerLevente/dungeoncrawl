30581003502cc8808684bfd88fc1bc4b67fca5dd
pannaincze <pannaincze@gmail.com>
2023-08-10 13:56:11 +0200

    Changes by Breez

--- src/main/java/com/codecool/dungeoncrawl/data/actors/enemies/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/enemies/Ghost.java
@@ -1,17 +1,9 @@
 package com.codecool.dungeoncrawl.data.actors.enemies;
 
 import com.codecool.dungeoncrawl.data.Cell;
-import com.codecool.dungeoncrawl.data.CellType;
-import com.codecool.dungeoncrawl.data.GameMap;
-import com.codecool.dungeoncrawl.logic.GameLogic;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
 public class Ghost extends Enemy{
     private int health = 6;
-
     private int damage = 25;
 
     public Ghost(Cell cell, String name) {
